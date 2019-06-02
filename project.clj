25104e3ffbc7b8cd967630a0c67817b5e6726341
ignorabilis <irina.yaroslavova@ignorabilis.com>
2019-06-02 17:40:09 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [cheshire "5.8.1"]
                  [clj-http "3.10.0"]
                  [org.bovinegenius/exploding-fish "0.3.6"]
-                 [cheshire "5.8.1"]
                  ;; TODO - remove this one
                  [com.auth0/auth0 "1.9.1"]]
   :repl-options {:port 1111})
