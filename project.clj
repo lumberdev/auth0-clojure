61316b1051864fc759e300a408c942d8779d32b0
ignorabilis <irina.yaroslavova@ignorabilis.com>
2019-06-24 11:11:20 +0300

    Remove snapshot from version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject auth0-clojure "0.1.0-SNAPSHOT"
+(defproject auth0-clojure "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
