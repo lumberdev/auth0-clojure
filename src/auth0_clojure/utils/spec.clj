9522f76bc4218c9737140bf417288fe5b6e4c632
ignorabilis <irina.yaroslavova@ignorabilis.com>
2019-07-01 11:53:19 +0300

    Auth uri spec init

--- src/auth0_clojure/utils/spec.clj
+++ src/auth0_clojure/utils/spec.clj
@@ -13,3 +13,7 @@
 
 (s/def :auth0/scope (s/* valid-scope?))
 
+;; TODO - scope should be possible to be set, not only list/vec
+(s/def :api/authorize-url
+  (s/keys :req [:auth0/client-id :auth0/redirect-uri :auth0/scope]
+          :opt [:auth0/state :auth0/audience :auth0/connection :auth0/response-type]))
