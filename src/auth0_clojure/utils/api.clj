a4e5a9d08339e69e0e67892aad119c5dedef5021
ignorabilis <irina.yaroslavova@ignorabilis.com>
2019-07-22 12:31:49 +0300

    API implementor - runtime path

--- src/auth0_clojure/utils/api.clj
+++ src/auth0_clojure/utils/api.clj
@@ -12,12 +12,38 @@
                        ;:body-validation nil
                        :headers     {:custom "random"}}})
 
-(defn create-api-call [[_ {:keys [:name :description :method]}]]
+(defn path->fn-args
+  "Converts the path provided to a destructuring map. Each keyword is considered
+  a parameter.
+  Example:
+  [\"connections\" :id] -> {id :id :as params}"
+  [path]
+  (let [args (->> path
+                  ;; TODO-param-val - implement parameters validation using a hash map
+                  (filter #(or (keyword? %) #_(map? %)))
+                  (map #(identity [(symbol %) %]))
+                  (into {}))
+        args (assoc args :as 'params)]
+    args))
+
+(defn runtime-path
+  "Matches params keys provided with their actual values at runtime."
+  [path params]
+  (map
+    #(if-let [param (get params %)]
+       ;; TODO-param-val - support hash map & validation here as well
+       param
+       %)
+    path))
+
+(defn create-api-call
+  "Creates an API function based on an EDN description."
+  [[name {:keys [:description :path]}]]
   (let []
     `(defn ~(symbol name)
        ~description
-       []
-       ~description)))
+       [~(path->fn-args path)]
+       (runtime-path ~path ~'params))))
 
 (defmacro implement-api
   "Given an EDN API description generates functions
@@ -27,5 +53,4 @@
   [api-description]
   (let [api-description (eval api-description)]
     `(do
-       ~@(map create-api-call
-              api-description))))
+       ~@(map create-api-call api-description))))
