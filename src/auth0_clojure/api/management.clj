eea5d27fddc4370945dae86978a20941bcb27da2
ignorabilis <irina.yaroslavova@ignorabilis.com>
2019-09-12 19:17:59 +0300

    Reorder todos

--- src/auth0_clojure/api/management.clj
+++ src/auth0_clojure/api/management.clj
@@ -2,10 +2,11 @@
   (:require [auth0-clojure.utils.data-api :as data-api]
             [auth0-clojure.descriptors.management :as mgmt-desc]))
 
-;; TODO - add exploration support, i.e. an fn to explore all available operations + a doc fn
 ;; TODO - add query params support, i.e. ?param1=xxx&param2=xxx
-;; TODO - fix the array issue
 ;; TODO - finish the api descriptor
+;; TODO - fix the array issue
+;; TODO - add exploration support, i.e. an fn to explore all available operations + a doc fn
+
 
 ;; TODO-lib - extract the fn that builds the request in a separate lib
 (defn invoke [config options]
