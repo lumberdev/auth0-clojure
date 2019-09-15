2dc1cd69c9195141cd12ce0e71eb1872a149a774
ignorabilis <irina.yaroslavova@ignorabilis.com>
2019-09-15 18:11:58 +0300

    Add query params support

--- src/auth0_clojure/api/management.clj
+++ src/auth0_clojure/api/management.clj
@@ -2,7 +2,6 @@
   (:require [auth0-clojure.utils.data-api :as data-api]
             [auth0-clojure.descriptors.management :as mgmt-desc]))
 
-;; TODO - add query params support, i.e. ?param1=xxx&param2=xxx
 ;; TODO - finish the api descriptor
 ;; TODO - fix the array issue
 ;; TODO - add exploration support, i.e. an fn to explore all available operations + a doc fn
--- src/auth0_clojure/utils/data_api.clj
+++ src/auth0_clojure/utils/data_api.clj
@@ -1,5 +1,6 @@
 (ns auth0-clojure.utils.data-api
   (:require [auth0-clojure.utils.requests :as req]
+            [auth0-clojure.utils.urls :as urls]
             [clojure.string :as string]))
 
 (defn api-descriptor-resolver [api-descriptor]
@@ -26,22 +27,27 @@
                     :as config}
                    {:keys [:operation
                            :path-params
+                           :query-params
                            :body]}]
   (let [api-descriptor (api-descriptor-resolver api-descriptor)
         {{:keys [method path headers]} :http} (operation-resolver api-descriptor operation)
-        path-prefix (get-in api-descriptor [:metadata :endpoint-prefix])
-        path        (map
-                      #(if (keyword? %)
-                         ;; TODO - throw exception if no such param???
-                         (get path-params %)
-                         %)
-                      path)
-        path        (str path-prefix (string/join "/" path))
-        req-data    (merge
-                      {:method  method
-                       :headers headers}
-                      (when body
-                        {:body body}))]
+        path-prefix    (get-in api-descriptor [:metadata :endpoint-prefix])
+        path-parts     (map
+                         #(if (keyword? %)
+                            ;; TODO - throw exception if no such path param???
+                            (get path-params %)
+                            %)
+                         path)
+        path           (str path-prefix (string/join "/" path-parts))
+        ;; TODO - throw exception if no such query param???
+        path           (if query-params
+                         (urls/build-url-params path query-params)
+                         path)
+        req-data       (merge
+                         {:method  method
+                          :headers headers}
+                         (when body
+                           {:body body}))]
     ;; TODO-lib - extract the fn that builds the request in a separate lib
     (req/auth0-mgmt-request
       config
