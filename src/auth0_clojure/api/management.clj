fe260d2aa40e689e182465745ae159368d489baf
ignorabilis <irina.yaroslavova@ignorabilis.com>
2019-09-16 15:30:50 +0300

    Update todos

--- src/auth0_clojure/api/management.clj
+++ src/auth0_clojure/api/management.clj
@@ -2,11 +2,11 @@
   (:require [auth0-clojure.utils.data-api :as data-api]
             [auth0-clojure.descriptors.management :as mgmt-desc]))
 
+;; TODO-lib - extract the fn that builds the request in a separate lib
+
 ;; TODO - fix the array issue
 ;; TODO - add exploration support, i.e. an fn to explore all available operations + a doc fn
-
-
-;; TODO-lib - extract the fn that builds the request in a separate lib
+;; TODO - some refactoring & cleanup
 (defn invoke [config options]
   (data-api/invoke-base
     (merge
