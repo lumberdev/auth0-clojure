b3ccfd464ad34bf479f22147cd914319b8c34a20
ignorabilis <irina.yaroslavova@ignorabilis.com>
2019-09-16 15:29:57 +0300

    Remove todo

--- src/auth0_clojure/api/management.clj
+++ src/auth0_clojure/api/management.clj
@@ -2,7 +2,6 @@
   (:require [auth0-clojure.utils.data-api :as data-api]
             [auth0-clojure.descriptors.management :as mgmt-desc]))
 
-;; TODO - finish the api descriptor
 ;; TODO - fix the array issue
 ;; TODO - add exploration support, i.e. an fn to explore all available operations + a doc fn
 
