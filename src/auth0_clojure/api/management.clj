aed9eca09257bb1632a86ca990777febc0041561
ignorabilis <irina.yaroslavova@ignorabilis.com>
2019-09-29 22:13:27 +0300

    Exploration support

--- src/auth0_clojure/api/management.clj
+++ src/auth0_clojure/api/management.clj
@@ -1,14 +1,64 @@
 (ns auth0-clojure.api.management
   (:require [auth0-clojure.utils.data-api :as data-api]
-            [auth0-clojure.descriptors.management :as mgmt-desc]))
+            [auth0-clojure.descriptors.management :as mgmt-desc]
+            [clojure.string :as string]))
 
-;; TODO-lib - extract the fn that builds the request in a separate lib
+;; TODO-lib - extract these fns in a separate lib
+;; since the actual descriptor could be something entirely different
 
-;; TODO - add exploration support, i.e. an fn to explore all available operations + a doc fn
 ;; TODO - some refactoring & cleanup
+;; TODO - this needs to be smthng like create-request
 (defn invoke [config options]
   (data-api/invoke-base
     (merge
       config
       {:api-descriptor mgmt-desc/api-descriptor})
     options))
+
+(defn op [op-key]
+  (-> mgmt-desc/api-descriptor
+      :operations
+      (get op-key)))
+
+(defn op-doc [op-key]
+  (let [{op-name :name
+         :keys [doc doc-url http]} (op op-key)
+        {:keys [path]} http
+        op-title (string/join " " (map string/capitalize (string/split (name op-name) #"-")))
+        path-params (seq (filter keyword? path))
+        query-params (seq [:filter])]
+    (string/join
+      "\n"
+      (cond-> ["##########################"
+               op-title
+               (str "(" op-name ")")
+               "##########################"]
+        doc
+        (into [""
+               doc])
+        doc-url
+        (into [""
+               "Official docs:"
+               doc-url])
+        path-params
+        (into [""
+               "-------------------------"
+               "Path Parameters:"
+               (string/join ", " path-params)])
+        query-params
+        (into [""
+               "-------------------------"
+               "Query Parameters:"
+               (string/join ", " query-params)])))))
+
+(defn update-values [m f & args]
+  (reduce
+    (fn [r [k v]]
+      (assoc r k (apply f v args)))
+    {}
+    m))
+
+(defn ops-list []
+  (update-values
+    (:operations mgmt-desc/api-descriptor)
+    :doc))
